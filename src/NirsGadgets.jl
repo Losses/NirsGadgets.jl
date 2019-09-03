3b61a16d5bf5472e1f4ae91c47fbb18cd348a16a
Losses Don <losses.don@gmail.com>
2019-09-03 21:05:21 +0800

    feat: Add rm_trd.

--- src/NirsGadgets.jl
+++ src/NirsGadgets.jl
@@ -10,10 +10,12 @@ include("./outliers.jl")
 include("./decimate.jl")
 include("./rm_glb.jl")
 include("./rm_spk.jl")
+include("./rm_trd.jl")
 
 export outliers
 export decimate
 export rm_glb
 export rm_spk
+export rm_trd
 
 end # module
