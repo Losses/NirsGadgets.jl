b76b331b50ff35b6494336ca494597111ed1517a
Losses Don <losses.don@gmail.com>
2019-09-03 10:30:29 +0800

    fix: oops!

--- src/check_data.jl
+++ src/check_data.jl
@@ -1,5 +1,5 @@
 function check(x::Matrix{T} where T <: Real)
-    r, c = size(x_)
+    r, c = size(x)
     if c > r
         @warn "the column size is greater than row size, you may need to transform your data"
     end
