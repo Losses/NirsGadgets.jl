152dbe52a126590ccdfebfad25e5084e7974679d
Losses Don <losses.don@gmail.com>
2019-09-11 17:21:15 +0800

    fix: Syntax mistake.

--- src/re_smp.jl
+++ src/re_smp.jl
@@ -6,8 +6,8 @@ function re_smp_deci(x::Neuro1DRealSignal; rate::Real)
     for i = 1:c
         res_c = resample(x.signal[:, i], rate)
 
-        isnothing(result) && result = zeros(length(res_c), c)
-        
+        isnothing(result) && (result = zeros(length(res_c), c))
+
         result[:, i] = res_c
     end
 
