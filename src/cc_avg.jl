89343cac95073d21c7202595a287d91896979b64
Losses Don <losses.don@gmail.com>
2019-09-19 11:09:03 +0800

    fix: Merge `cc_avg`.

--- src/cc_avg.jl
+++ src/cc_avg.jl
@@ -1,8 +1,8 @@
-function cc_avg(x::Neuro1DRealSignal)::Neuro1DRealSignal
-    cc_avg(x, channels = [1:1:size(x)[2];])
-end
+function cc_avg(x::Neuro1DRealSignal; channels::Union{Vector{Int64}, Nothing} = nothing)::Neuro1DRealSignal
+    if (channels == nothing)
+        return cc_avg(x, channels = [1:1:size(x.signal)[2];])
+    end
 
-function cc_avg(x::Neuro1DRealSignal; channels::Vector{Int64})::Neuro1DRealSignal
     Neuro1DRealSignal(
         signal = Statistics.mean(x.signal[:, channels]; dims = 2),
         sample_rate = x.sample_rate
